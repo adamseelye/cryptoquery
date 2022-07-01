62d1af5b868e33c86a7c6e7a544c6a5ae2e73e88
adamseelye <adam945@revature.net>
2022-06-30 21:52:24 -0600

    created db connection class

---
+++ src/main/scala/connector.scala
@@ -0,0 +1,28 @@
+import java.sql.SQLException
+import java.sql.Connection
+import java.sql.ResultSet
+import java.sql.Statement
+import java.sql.DriverManager
+
+class connector extends App {
+  def hiveCxn (args: Array[String]): Unit = {
+    val hiveDriver = "org.apache.hadoop.hive.jdbc.HiveDriver"
+
+    // Set connection and SQL variable
+    val cxn = DriverManager.getConnection("jdbc:hive2://<address>:<port>/<dir>", "", ""
+    val stmt = cxn.createStatement()
+    val table = "testTable"
+
+    // Create table
+    stmt.executeQuery(s"DROP TABLE ${table} IF EXISTS;")
+    val res  = stmt.executeQuery(s"CREATE TABLE ${table}" + " (KEY int, VALUE string);")
+
+    // Select query
+    val sql = s"SELECT * FROM ${table};"
+    res = stmt.executeQuery(sql)
+    while (res.next()) {
+      println(res.getString(1))
+
+    }
+  }
+}
